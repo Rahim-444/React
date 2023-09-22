6a8368b75990483a0ff26021b8d3163dfac8cb20
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2023-09-23 00:19:49 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Board } from "./components/board";
 import { Win } from "./components/Winboard";
 
@@ -62,24 +62,70 @@ function App() {
             setBoard(Array(9).fill(null));
         }
     };
+    const [eqS, seteqS] = useState(false);
     const handleClick = (index) => {
         const UpdateBoard = board.map((value, ind) => {
             if (index === ind) {
-                Setxplay(!xplay);
-                if (xplay & (value === null)) {
-                    return "X";
-                } else if (!xplay & (value === null)) {
-                    return "O";
+                if (eqS === true) {
+                    Setxplay(!xplay);
+                    if (xplay & (value === null)) {
+                        return "X";
+                    } else if (!xplay & (value === null)) {
+                        return "O";
+                    }
                 }
             }
             return value;
         });
         setBoard(UpdateBoard);
     };
+    const makeEquation = () => {
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let operators = ["+", "-", "*"];
+        let firstNumber = numbers[Math.floor(Math.random() * 9)];
+        let secondNumber = numbers[Math.floor(Math.random() * 9)] * 10;
+        let operator = operators[Math.floor(Math.random() * 3)];
+        let Equation = `${firstNumber} ${operator} ${secondNumber}`;
+        return Equation;
+    };
+    const [equation, setEquation] = useState(makeEquation());
+    useEffect(() => {
+        setEquation(makeEquation());
+    }, []);
+
+    const [res, setres] = useState(null);
+    const change = (event) => {
+        setres(event.target.value);
+    };
+    let equationsolved;
+    const Fclick = () => {
+        let answer = eval(equation);
+        let userAnswer = parseInt(res);
+
+        if (userAnswer !== answer) {
+            Setxplay(!xplay);
+            seteqS(false);
+        } else {
+            seteqS(true);
+        }
+
+        // Generate a new equation after checking the user's answer
+        setEquation(makeEquation());
+        setres(""); // Clear the input field
+    };
     return (
         <div className="App">
             <div className="equation-x">
-                <h1>Equation X</h1>
+                {!xplay ? (
+                    <h1>Wait for your turn</h1>
+                ) : (
+                    <div>
+                        <h1>Equation X</h1>
+                        <h2>{equation}</h2>
+                        <input type="text" value={res} onChange={change} />
+                        <button onClick={Fclick}>Submit</button>
+                    </div>
+                )}
             </div>
             <div className="tic">
                 <Win xWinnings={xCount} oWinnings={oCount} />
@@ -90,7 +136,16 @@ function App() {
                 {HandleWin(board)}
             </div>
             <div className="equation-o">
-                <h1>Equation O</h1>
+                {xplay ? (
+                    <h1>Wait for your turn</h1>
+                ) : (
+                    <div>
+                        <h1>Equation O</h1>
+                        <h2>{equation}</h2>
+                        <input type="text" value={res} onChange={change} />
+                        <button onClick={Fclick}>Submit</button>
+                    </div>
+                )}
             </div>
         </div>
     );
